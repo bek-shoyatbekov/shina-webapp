d6f26a1fc54b04517a254876d1fb9c6a3e0c9939
bek <shoyatbekov03032003@gmail.com>
2023-11-07 16:07:47 +0500

    seperated admin and client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import Product from './components/Product';
-import { AdminHome } from './pages/Admin';
-import AddProduct from './components/AddProduct';
-import EditProduct from './components/EditProduct';
-import Login from './pages/AdminLogin';
 
 
 function App() {
@@ -14,10 +10,6 @@ function App() {
         <Routes>
           <Route path={'/'} element={<Home />} />
           <Route path={'/wheel'} element={<Product />} />
-          <Route path={'/admin'} element={<Login />} />
-          <Route path={'/admin/home'} element={< AdminHome />} />
-          <Route path={'/admin/add'} element={< AddProduct />} />
-          <Route path={'/admin/edit'} element={< EditProduct />} />
         </Routes>
       </BrowserRouter>
     </>
