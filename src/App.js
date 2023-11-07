313b2911f994d7e46ee3726ca5a33d8d1a48788e
bek <shoyatbekov03032003@gmail.com>
2023-11-07 19:12:11 +0500

    with tele.ready()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import Product from './components/Product';
+import { useEffect } from 'react';
+
+
+const tele = window.Telegram.WebApp;
 
 
 function App() {
+
+  useEffect(() => {
+    tele.ready();
+  })
+
   return (
     <>
       <BrowserRouter>
